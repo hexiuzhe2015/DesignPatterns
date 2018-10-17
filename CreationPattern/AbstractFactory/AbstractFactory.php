e3852800e17d9400b8e48c8245fead6792166606
hexiuzhe <hexiuzhe_2014@163.com>
2018-10-17 23:18:49 +0800

    recommit

--- CreationPattern/AbstractFactory/AbstractFactory.php
+++ CreationPattern/AbstractFactory/AbstractFactory.php
@@ -5,3 +5,51 @@
  * Date: 2018/10/17
  * Time: 22:25
  */
+
+namespace DesignPatterns\CreationalPattern\AbstractFactory;
+
+class Button{}
+class Border{}
+
+class WinButton extends Button{}
+class WinBorder extends Border{}
+class MacButton extends Button{}
+class MacBorder extends Border{}
+
+abstract class AbstractFactory
+{
+    abstract public function CreateButton();
+    abstract public function CreateBorder();
+}
+
+class WinFactory extends AbstractFactory
+{
+
+    public function CreateButton()
+    {
+        // TODO: Implement CreateButton() method.
+        return new WinButton();
+    }
+
+    public function CreateBorder()
+    {
+        // TODO: Implement CreateBorder() method.
+        return new WinBorder();
+    }
+}
+
+class MacFactory extends AbstractFactory
+{
+
+    public function CreateButton()
+    {
+        // TODO: Implement CreateButton() method.
+        return new MacButton();
+    }
+
+    public function CreateBorder()
+    {
+        // TODO: Implement CreateBorder() method.
+        return new MacBorder();
+    }
+}
