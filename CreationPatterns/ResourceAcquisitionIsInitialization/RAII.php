bb8930498442c4aceb5fd787b9b3afd81d1f0c51
hexiuzhe <hexiuzhe_2014@163.com>
2018-10-25 10:55:08 +0800

    RAII

---
+++ CreationPatterns/ResourceAcquisitionIsInitialization/RAII.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 贺秀哲
+ * Date: 2018/10/25
+ * Time: 10:36
+ */
+
+namespace DesignPatterns\CreationalPatterns\RAII;
+
+class RAII
+{
+    public function __construct()
+    {
+    }
+
+    public function __destruct()
+    {
+        // TODO: Implement __destruct() method.
+    }
+}
--- StructuralPatterns/Adapter/Adapter.php
+++ StructuralPatterns/Adapter/Adapter.php
@@ -7,3 +7,4 @@
  */
 
 namespace DesignPatterns\StructuralPatterns\Adapter;
+
